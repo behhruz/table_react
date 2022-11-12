eaa43b0258710ea235bc06c57d8fb332d06ec762
behhruz <bsatimbayev05@gmail.com>
2022-11-12 18:45:26 +0500

    finished project

---
+++ src/data.jsx
@@ -0,0 +1,85 @@
+export const g6 = [
+  {
+    id: 1,
+    name: "Elyorbek",
+    age: 16,
+    job: "developer",
+  },
+  {
+    id: 2,
+    name: "Abdulaziz",
+    age: 17,
+    job: "developer",
+  },
+  {
+    id: 3,
+    name: "Begzod",
+    age: 25,
+    job: "developer",
+  },
+
+  {
+    id: 4,
+    name: "Munisa",
+    age: 14,
+    job: "developer",
+  },
+
+  {
+    id: 5,
+    name: "Dilshoda",
+    age: 15,
+    job: "developer",
+  },
+
+  {
+    id: 6,
+    name: "Javohir",
+    age: 19,
+    job: "developer",
+  },
+
+  {
+    id: 7,
+    name: "Kamoliddin",
+    age: 22,
+    job: "developer",
+  },
+
+  {
+    id: 8,
+    name: "Husniddin",
+    age: 16,
+    job: "developer",
+  },
+  {
+    id: 9,
+    name: "Kamron",
+    age: 18,
+    job: "developer",
+  },
+  {
+    id: 10,
+    name: "Shamshod",
+    age: 17,
+    job: "developer",
+  },
+  {
+    id: 11,
+    name: "Dilmurod",
+    age: 15,
+    job: "developer",
+  },
+  {
+    id: 12,
+    name: "Sayfiddin",
+    age: 23,
+    job: "developer",
+  },
+  {
+    id: 13,
+    name: "Behruz",
+    age: 17,
+    job: "developer",
+  },
+];
--- src/table.jsx
+++ src/table.jsx
@@ -1,142 +1,81 @@
 import React from "react";
 import "./table.css";
+import { g6 } from "./data";
 
 class Students extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      g6: [
-        {
-          id: 1,
-          name: "Elyorbek",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 2,
-          name: "Abdulaziz",
-          age: 17,
-          job: "developer",
-        },
-        {
-          id: 3,
-          name: "Begzod",
-          age: 25,
-          job: "developer",
-        },
-
-        {
-          id: 4,
-          name: "Munisa",
-          age: 14,
-          job: "developer",
-        },
-
-        {
-          id: 5,
-          name: "Dilshoda",
-          age: 15,
-          job: "developer",
-        },
-
-        {
-          id: 6,
-          name: "Javohir",
-          age: 19,
-          job: "developer",
-        },
-
-        {
-          id: 7,
-          name: "Kamoliddin",
-          age: 22,
-          job: "developer",
-        },
-
-        {
-          id: 8,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 9,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 10,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 11,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 12,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-        {
-          id: 13,
-          name: "Husniddin",
-          age: 16,
-          job: "developer",
-        },
-      ],
+      data: g6,
+      name: "",
+      age: "",
+      job: "",
     };
   }
   render() {
+    // delete function
     const onDelete = (id) => {
-      let res = this.state.g6.filter((v) => v.id !== id);
-      this.setState({ g6: res });
+      let res = this.state.data.filter((v) => v.id !== id);
+      this.setState({ data: res });
     };
+    // edit function
     const onEdit = () => {};
+
+    // search function
+    const filter = ({ target: { value } }) => {
+      let res = g6.filter((v) =>
+        v.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      );
+      this.setState({ data: res });
+    };
+    const onChangeInput = ({ target: { value, name } }) => {
+      this.setState({ [name]: value });
+    };
+    // add function
     const onAdd = () => {
-      let user = {
-        id: this.state.g6.length + 1,
-        name: this.state.name,
-        age: this.state.age,
-        job: this.state.job,
-      };
-      this.setState({
-        g6: [...this.state.g6, user],
-        name: "",
-        age: "",
-        job: "",
-      });
+      let users = [
+        ...this.state.data,
+        {
+          id: this.state.data.length + 1,
+          name: this.state.name,
+          age: this.state.age,
+          job: this.state.job,
+        },
+      ];
+      return this.setState({ data: users });
     };
     return (
       <div>
         <div className="bigtable">
-        <div className="inputs">
-     <input
-          value={this.state.name}
-          className="input"
-          type="text"
-          placeholder="name"
-          name="name"
-        />
-        <input
-          value={this.state.age}
-          type="number"
-          placeholder="age"
-          name="age"
-        />
-        <input
-          value={this.state.job}
-          type="text"
-          placeholder="job"
-          name="job"
-        />
-        <button onClick={onAdd}>add</button>
-     </div>
+          <div className="inputs">
+            <input
+              onChange={onChangeInput}
+              className="input"
+              type="text"
+              placeholder="name"
+              name="name"
+            />
+            <input
+              onChange={onChangeInput}
+              type="number"
+              placeholder="age"
+              name="age"
+            />
+            <input
+              onChange={onChangeInput}
+              type="text"
+              placeholder="job"
+              name="job"
+            />
+            <button onClick={onAdd}>add</button>
+            <input
+              onChange={filter}
+              className="inp"
+              type="text"
+              name="name"
+              placeholder="search..."
+            />
+          </div>
           <table className="table" width={"90%"}>
             <thead className="fixed-tr">
               <tr className="fixed-tr">
@@ -144,26 +83,25 @@ class Students extends React.Component {
                 <th className="th">NAME</th>
                 <th className="th">AGE</th>
                 <th className="th">JOb</th>
-                <th className="th">DELETE</th>
-                <th className="th">EDIT</th>
+                <th className="th">Actions</th>
               </tr>
             </thead>
             <tbody className="tbody">
-              {this.state.g6.map(({ id, name, age, job }) => {
+              {this.state.data.map((st,index) => {
                 return (
-                  <tr key={id}>
-                    <td className="asd id-sticky">{id}</td>
-                    <td className="qwe">{name}</td>
-                    <td className="zxc">{age}</td>
-                    <td className="rfv">{job}</td>
+                  <tr key={st.id}>
+                    <td className="asd id-sticky">{st.id}</td>
+                    <td className="qwe">{st.name}</td>
+                    <td className="zxc">{st.age}</td>
+                    <td className="rfv">{st.job}</td>
                     <td>
-                      <button onClick={() => onDelete(id)} className="pli">
+                      <button onClick={() => onDelete(st.id)} className="pli">
                         delete
                       </button>
                     </td>
                     <td>
                       <button
-                        onClick={() => onEdit({ name, age, job })}
+                     onClick={onEdit(st.id)}
                         className="edit_sticy"
                       >
                         edit
